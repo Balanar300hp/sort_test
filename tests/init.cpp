2497ce6607a73e18cff0bdd1614e4a8ffe454e96
Balanar300hp <kudashov.vlad@yandex.ru>
2016-11-21 13:05:55 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -9,22 +9,24 @@ using namespace std;
 SCENARIO("count111", "[count1111]"){
  setlocale(LC_ALL, "Russian");
  B obj("name.txt",1048576);
- ifstream hay("out.txt"),file("sort.txt");
+ ifstream hay("out.txt");
+ ofstream file("sort.txt");
 
 int i=0;
  bool p=true;
  string s1,s2;
- while (!file.eof()&&!hay.eof()){
- getline(file,s1);
+ while (!hay.eof()){
+// getline(file,s1);
  getline(hay,s2);
   i++;
-  if (s1!=s2){
+  /*if (s1!=s2){
   p=false;
    cout<<i<<endl;
    cout<<s1<<endl;
    cout<<s2<<endl;
    break;
-  }
+  }*/
+  file<<s2<<endl;
  }
   file.close();
  hay.close();
