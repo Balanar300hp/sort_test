ae3ddd145d906ec551dc7d9628020b2e5777c19c
Balanar300hp <kudashov.vlad@yandex.ru>
2016-11-21 11:35:14 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -8,7 +8,7 @@ using namespace std;
  
 SCENARIO("count111", "[count1111]"){
  setlocale(LC_ALL, "Russian");
-  B obj("names.txt");
+ B obj("names.txt",1048576);
  ifstream file("sort.txt");
  ifstream ff("out.txt");
 
