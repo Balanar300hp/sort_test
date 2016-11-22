30111cca9a693b941ecca4da139bdf0598ac2e57
Balanar300hp <kudashov.vlad@yandex.ru>
2016-11-23 01:56:03 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -6,7 +6,7 @@
 #include <locale> 
 using namespace std;
  
-SCENARIO("count111", "[count1111]"){
+SCENARIO("32mb", "[32mb]"){
  setlocale(LC_ALL, "Russian");
  B obj("32","out",32);
  ifstream hay("out");
@@ -31,3 +31,57 @@ getline(file,s1);
  hay.close();
   REQUIRE(p==true);
 }
+
+SCENARIO("15mb", "[15mb]"){
+ setlocale(LC_ALL, "Russian");
+ B obj("15","out2",15);
+ ifstream hay("out2");
+ifstream file("sort2");
+
+int i=0;
+ bool p=true;
+ string s1,s2;
+ while (!hay.eof()&&!file.eof()){
+getline(file,s1);
+ getline(hay,s2);
+  i++;
+  if (s1!=s2){
+  p=false;
+   cout<<i<<endl;
+   cout<<s1<<endl;
+   cout<<s2<<endl;
+   break;
+  }
+ }
+  file.close();
+ hay.close();
+  REQUIRE(p==true);
+}
+
+SCENARIO("8mb", "[8mb]"){
+ setlocale(LC_ALL, "Russian");
+ B obj("8","out3",8);
+ ifstream hay("out3");
+ifstream file("sort3");
+
+int i=0;
+ bool p=true;
+ string s1,s2;
+ while (!hay.eof()&&!file.eof()){
+getline(file,s1);
+ getline(hay,s2);
+  i++;
+  if (s1!=s2){
+  p=false;
+   cout<<i<<endl;
+   cout<<s1<<endl;
+   cout<<s2<<endl;
+   break;
+  }
+ }
+  file.close();
+ hay.close();
+  REQUIRE(p==true);
+}
+
+
